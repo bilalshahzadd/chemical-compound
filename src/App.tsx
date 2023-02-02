a0a3eeaad47cee1eb0a6e978716d50cf0ab12cf7
bilalshahzadd <bilalshahzad521@gmail.com>
2023-02-02 21:06:42 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+interface PeriodicTable {
+  [key: string]: string;
+}
+
+const periodicTable: PeriodicTable = {
+  'H': 'Hydrogen',
+  'He': 'Helium',
+  'Li': 'Lithium',
+  'Na': 'Sodium'
+};
+
+const App: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const inputArray = input.split(' ');
+    let outputArray: any = [];
+    let isPossible = true;
+
+    inputArray.forEach(input => {
+      if (periodicTable[input]) {
+        outputArray.push(input);
+      } else {
+        isPossible = false;
+      }
+    });
+
+    setOutput(isPossible ? outputArray.join(' ') : 'Not possible');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={handleChange} />
+        <button type="submit">Convert</button>
+      </form>
+      <div>{output}</div>
     </div>
   );
-}
+};
 
 export default App;
